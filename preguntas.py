8562769cca4eba03c81ae2d9d8c74e5ec9b0026c
ATHidalgo <ahidalgoa@unal.edu.co>
2023-11-14 15:07:40 -0500

    4

--- preguntas.py
+++ preguntas.py
@@ -90,7 +90,7 @@ def pregunta_03():
     # un vector con valores entre el máximo y el mínimo de X_fertility
     prediction_space = np.linspace(
         X_fertility.min(),
-        X_fertility.max(),
+        X_fertility.max()
     ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
